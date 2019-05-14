75b10d795733bd185f21b3901ab6804741f30509
Wenxuan <wenxuangm@gmail.com>
2019-05-14 23:53:08 +0800

    Add progress bar support.

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,18 @@ extern crate serde_json;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ScrollResponse {
     pub _scroll_id: String,
-    pub took: i32,
-    pub hits: Hits,
+    pub took:       i32,
+    pub hits:       Hits,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Hits {
-    pub total: i64,
+    pub total:     i64,
     pub max_score: f64,
-    pub hits: Vec<Hit>,
+    pub hits:      Vec<Hit>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Hit {
-    pub _source: serde_json::Value
+    pub _source: serde_json::Value,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
+extern crate elastic_tunnel_rs;
+
 extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
 
-extern crate elastic_tunnel_rs;
-
 use elastic_tunnel_rs::ScrollResponse;
 
 use crossbeam::crossbeam_channel;
-use std::thread;
-use serde_json::Value;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use reqwest::StatusCode;
-use std::io::BufReader;
-use structopt::StructOpt;
-use std::path::PathBuf;
 use std::fs::File;
+use std::io::{BufReader, Write};
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::thread;
+use structopt::StructOpt;
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let opt = Opt::from_args();
@@ -21,72 +22,96 @@ fn main() -> Result<(), Box<std::error::Error>> {
     let index = opt.index;
     let slice = opt.slice;
     let batch: Option<u32> = opt.batch;
+    let scroll_ttl = opt.scroll_ttl;
 
     let query = BufReader::new(File::open(opt.query)?);
     let query: serde_json::Value = serde_json::from_reader(query)?;
 
-    let (s, r) = crossbeam_channel::bounded(0);
+    let (tx, rx) = crossbeam_channel::bounded(0);
 
+    let mpb = Arc::new(MultiProgress::new());
     let mut children = vec![];
     for slice_id in 0..slice {
-        let s = s.clone();
+        let tx = tx.clone();
         let mut query = query.clone();
         let host = host.clone();
         let index = index.clone();
+        let scroll_ttl = scroll_ttl.clone();
+
+        let mpb = mpb.clone();
+        let pb = mpb.add(ProgressBar::new_spinner());
+        let style = ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .progress_chars("##-");
+        pb.set_style(style);
+        pb.set_message(&format!("Slice {}", slice_id));
+
         children.push(thread::spawn(move || {
             let client = reqwest::Client::new();
-
             if slice > 1 {
                 let obj = query.as_object_mut().unwrap();
-                obj.insert("slice".into(), json!({
-                    "id": slice_id,
-                    "max": slice
-                }));
+                obj.insert(
+                    "slice".into(),
+                    json!({
+                        "id": slice_id,
+                        "max": slice
+                    }),
+                );
                 query = json!(obj);
             }
 
             let mut params = vec![("scroll", "1m".to_owned())];
             match batch {
                 Some(batch) => params.push(("size", batch.to_string())),
-                None => (),
+                None => {}
             }
             let mut res = client
                 .post(&format!("{}/{}/_search", &host, &index))
                 .query(&params)
                 .json(&query)
-                .send().expect("error sending request");
+                .send()
+                .expect("error sending request");
 
             if res.status() != 200 {
-                eprintln!("error query es. status={}, content={}", res.status(), res.text().unwrap());
+                eprintln!(
+                    "error query es. status={}, content={}",
+                    res.status(),
+                    res.text().unwrap()
+                );
                 return;
             }
 
-            let res: ScrollResponse = res
-                .json().expect("error parsing response");
+            let res: ScrollResponse = res.json().expect("error parsing response");
+
+            pb.set_length(res.hits.total as u64);
+            pb.inc(res.hits.hits.len() as u64);
 
             let mut scroll_id = res._scroll_id.clone();
             let mut finished = res.hits.hits.is_empty();
-            s.send(res).expect("error sending result to channel");
+            tx.send(res).expect("error sending result to channel");
 
             while !finished {
                 let mut res = reqwest::Client::new()
-                    .post("http://47.93.125.169:9200/_search/scroll")
+                    .post(&format!("{}/_search/scroll", &host))
                     .json(&json!({
-                        "scroll": "1m",
+                        "scroll": scroll_ttl,
                         "scroll_id": scroll_id,
                     }))
-                    .send().expect("error sending request");
+                    .send()
+                    .expect("error sending request");
 
                 if res.status() != StatusCode::OK {
                     eprintln!("error query es: {:?}", res);
                     return;
                 }
-                let res: ScrollResponse = res
-                    .json().expect("error parsing response");
+                let res: ScrollResponse = res.json().expect("error parsing response");
+                pb.inc(res.hits.hits.len() as u64);
                 scroll_id = res._scroll_id.clone();
                 finished = res.hits.hits.is_empty();
-                s.send(res).expect("error sending result to channel");
+                tx.send(res).expect("error sending result to channel");
             }
+
+            pb.finish_with_message(&format!("Slice {} finished", slice_id))
         }));
     }
 
@@ -94,20 +119,30 @@ fn main() -> Result<(), Box<std::error::Error>> {
         for child in children {
             child.join().expect("error joining");
         }
-        drop(s);
+        drop(tx);
     });
 
-    for res in r {
-        for hit in res.hits.hits {
-            println!("{}", json!(hit._source));
+    let output = opt.output;
+    thread::spawn(move || {
+        let mut output: Box<Write> = match output {
+            Some(output) => Box::new(File::create(output).unwrap()),
+
+            None => Box::new(std::io::stdout()),
+        };
+        for res in rx {
+            for hit in res.hits.hits {
+                writeln!(&mut output, "{}", json!(hit._source)).unwrap();
+            }
         }
-    }
+    });
+
+    mpb.join_and_clear().unwrap();
     Ok(())
 }
 
-
 #[derive(StructOpt, Debug)]
-#[structopt(name = "basic")]
+#[structopt(name = "elastic-tunnel")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
 struct Opt {
     #[structopt(short = "h", long = "host", default_value = "http://localhost:9200")]
     host: String,
@@ -123,4 +158,10 @@ struct Opt {
 
     #[structopt(short = "b", long = "batch")]
     batch: Option<u32>,
+
+    #[structopt(short = "o", long = "output")]
+    output: Option<PathBuf>,
+
+    #[structopt(long = "scroll-ttl", default_value = "1m")]
+    scroll_ttl: String,
 }
