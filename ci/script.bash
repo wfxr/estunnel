2c40d559ae3344658bd455087ef43741b3e19339
Wenxuan Zhang <wenxuangm@gmail.com>
2019-05-16 10:41:10 +0800

    Improve ci.

--- ci/script.bash
+++ ci/script.bash
@@ -5,11 +5,11 @@ set -ex
 # Incorporate TARGET env var to the build and test process
 if [[ $TARGET == x86_64-unknown-linux-musl ]]; then
     # TODO: run test after build
-    docker run -v "$PWD:/volume" "clux/muslrust:$RUST_VERSION" cargo build --target "$TARGET" --release
+    docker run -v "$PWD:/volume" "clux/muslrust:$DOCKER_RUST_VERSION" cargo build --target "$TARGET" --release
 else
     # We cannot run arm executables on linux
     if [[ $TARGET != arm-unknown-linux-* ]]; then
-        cargo test --target "$TARGET" --verbose
+        cargo test --target "$TARGET"
     fi
     cargo build --target "$TARGET" --release
 fi
