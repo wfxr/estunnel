37c3b1151821373ee296a9b7f1c4b346f7ad5e03
Wenxuan Zhang <wenxuangm@gmail.com>
2019-05-16 12:52:49 +0800

    Fix ci config.

--- ci/before_install.bash
+++ ci/before_install.bash
@@ -6,14 +6,8 @@ if [ "$TRAVIS_OS_NAME" != linux ]; then
     exit 0
 fi
 
-sudo apt update
-
-sudo apt install -y jq
-
-PROJECT_NAME=$(cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].name')
-export PROJECT_NAME
-
 # needed for i686 linux gnu target
 if [[ $TARGET == i686-unknown-linux-gnu ]]; then
+    sudo apt update
     sudo apt install -y gcc-multilib
 fi
