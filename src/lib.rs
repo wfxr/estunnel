fa5ac40228a0ba136cfcc583ad3fe24b6f0dec35
Wenxuan Zhang <wenxuangm@gmail.com>
2019-05-17 10:31:00 +0800

    Refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,21 @@
 extern crate serde_derive;
 extern crate serde_json;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct ScrollResponse {
     pub _scroll_id: String,
-    pub took:       i32,
+    pub took:       u32,
     pub hits:       Hits,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct Hits {
-    pub total:     i64,
+    pub total:     u64,
     pub max_score: f64,
     pub hits:      Vec<Hit>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct Hit {
     pub _source: serde_json::Value,
 }
