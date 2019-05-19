32a8e648cefbc94c2f2b3a561dd1a2495fba481d
Wenxuan <wenxuangm@gmail.com>
2019-05-19 18:00:05 +0800

    Async attempt.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
-extern crate estunnel;
-
-extern crate serde_derive;
-#[macro_use]
-extern crate serde_json;
-
 use estunnel::ScrollResponse;
 
-use crossbeam::crossbeam_channel;
+use crossbeam::crossbeam_channel::{self, Sender};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use reqwest::Response;
-use std::cmp::{max, min};
+use serde_json::json;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Write};
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::thread;
 use structopt::StructOpt;
+use threadpool::ThreadPool;
 
-const CHANNEL_CAPACITY: usize = 10000;
+const CHANNEL_CAPACITY: usize = 10;
+
+struct Package {
+    pub slice_id: u32,
+    pub total:    u64,
+    pub docs:     Vec<String>,
+}
+
+struct MPB {
+    pub mpb: Arc<MultiProgress>,
+    pbs:     HashMap<u32, ProgressBar>,
+    size:    usize,
+    joined:  bool,
+}
+
+impl MPB {
+    fn new(size: usize) -> MPB {
+        MPB {
+            mpb: Arc::new(MultiProgress::new()),
+            pbs: HashMap::new(),
+            size,
+            joined: false,
+        }
+    }
+    fn inc(&mut self, id: u32, delta: u64, total: u64) {
+        if self.pbs.contains_key(&id) {
+            self.pbs[&id].set_length(total);
+            self.pbs[&id].inc(delta)
+        } else {
+            let pb = self.mpb.add(ProgressBar::new(total));
+            let style = ProgressStyle::default_bar()
+                .template("{prefix} [{elapsed_precise}] {bar:60.cyan/blue} {msg} {pos:>7}/{len:7} (ETA {eta_precise})")
+                .progress_chars("##-");
+            pb.set_style(style);
+            pb.set_prefix(&format!("Slice-{}", id));
+            pb.set_message("Running...");
+            pb.inc(delta);
+            self.pbs.insert(id, pb);
+        }
+        if !self.joined && self.pbs.len() >= self.size {
+            self.joined = true;
+            let mpb = self.mpb.clone();
+            thread::spawn(move || {
+                mpb.join_and_clear().unwrap();
+            });
+        }
+    }
+
+    fn finish_with_message(&self, msg: &str) {
+        for pb in self.pbs.values() {
+            pb.finish_with_message(msg)
+        }
+    }
+}
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let opt = Opt::from_args();
@@ -41,8 +90,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     let (tx, rx) = crossbeam_channel::bounded(CHANNEL_CAPACITY);
 
-    let mpb = Arc::new(MultiProgress::new());
-    let mut producer_threads = vec![];
+    let io_pool = ThreadPool::new(slice as usize);
+    let compute_pool = ThreadPool::new(num_cpus::get());
     for slice_id in 0..slice {
         let tx = tx.clone();
         let mut query = query.clone();
@@ -52,16 +101,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
         let user = user.clone();
         let pass = pass.clone();
 
-        let mpb = mpb.clone();
-        let pb = mpb.add(ProgressBar::new(1));
-        let style = ProgressStyle::default_bar()
-            .template("{prefix} [{elapsed_precise}] {bar:60.cyan/blue} {msg}")
-            .progress_chars("##-");
-        pb.set_style(style);
-        pb.set_prefix(&format!("Slice-{}", slice_id));
-        pb.set_message("Starting...");
-
-        producer_threads.push(thread::spawn(move || {
+        let compute_pool = compute_pool.clone();
+        io_pool.execute(move || {
             let client = reqwest::Client::new();
             if slice > 1 {
                 let obj = query.as_object_mut().unwrap();
@@ -87,19 +128,12 @@ fn main() -> Result<(), Box<std::error::Error>> {
                 .send()
                 .expect("error sending request");
 
-            let (docs, mut scroll_id, total) = parse_response(res).expect("error parsing response");
-
-            let style = ProgressStyle::default_bar()
-                .template("{prefix} [{elapsed_precise}] {bar:60.cyan/blue} {msg} {pos:>7}/{len:7} (ETA {eta_precise})")
-                .progress_chars("##-");
-            pb.set_message("Running...");
-            pb.set_style(style);
-            pb.set_length(total);
-            pb.set_draw_delta(max(1, min(10000, total / 1000)));
-            pb.inc(docs.len() as u64);
-
-            let mut finished = docs.is_empty();
-            tx.send(Box::new(docs)).expect("error sending result to channel");
+            let tx1: Sender<Box<Package>> = tx.clone();
+            let pool = compute_pool.clone();
+            let (mut scroll_id, mut finished) = parse_response(slice_id, res, pool, move |package| {
+                tx1.send(package).expect("error sending result to channel");
+            })
+            .expect("error parsing response");
 
             while !finished {
                 let res = client
@@ -112,23 +146,21 @@ fn main() -> Result<(), Box<std::error::Error>> {
                     .send()
                     .expect("error sending request");
 
-                let (docs, new_scroll_id, total) = parse_response(res).expect("error parsing response");
+                let tx1 = tx.clone();
+                let pool = compute_pool.clone();
+                let (new_scroll_id, new_finished) = parse_response(slice_id, res, pool, move |package| {
+                    tx1.send(package).expect("error sending result to channel");
+                })
+                .expect("error parsing response");
 
                 scroll_id = new_scroll_id;
-                pb.set_length(total);
-                pb.inc(docs.len() as u64);
-                finished = docs.is_empty();
-                tx.send(Box::new(docs)).expect("error sending result to channel");
+                finished = new_finished;
             }
-
-            pb.finish_with_message("Finished.")
-        }));
+        });
     }
 
-    thread::spawn(|| {
-        for th in producer_threads {
-            th.join().expect("error joining");
-        }
+    thread::spawn(move || {
+        io_pool.join();
         drop(tx);
     });
 
@@ -138,28 +170,65 @@ fn main() -> Result<(), Box<std::error::Error>> {
             Some(output) => Box::new(File::create(output).unwrap()) as Box<Write>,
             None => Box::new(std::io::stdout()),
         });
-        for docs in rx.iter() {
-            for doc in docs.iter() {
+
+        let mut mpb = MPB::new(slice as usize);
+
+        for pack in rx.iter() {
+            mpb.inc(pack.slice_id, pack.docs.len() as u64, pack.total);
+            for doc in pack.docs.iter() {
                 writeln!(&mut output, "{}", doc).unwrap();
             }
         }
+        mpb.finish_with_message("Finished.");
     });
 
-    mpb.join_and_clear().unwrap();
     consumer_thread.join().unwrap();
     Ok(())
 }
 
-fn parse_response(mut res: Response) -> Result<(Vec<String>, String, u64), Box<std::error::Error>> {
+fn parse_response<F: Fn(Box<Package>) -> ()>(
+    slice_id: u32,
+    mut res: Response,
+    pool: ThreadPool,
+    f: F,
+) -> Result<(String, bool), Box<std::error::Error>>
+where
+    F: std::marker::Send + 'static,
+{
     if res.status() != 200 {
         return Err(format!("error query es. status={}, content={}", res.status(), res.text()?).into());
     }
     // serde_json has bad performance on reader. So we first read body into a string.
     // See: https://github.com/serde-rs/json/issues/160
     let res = res.text()?;
-    let res: ScrollResponse = serde_json::from_str(&res)?;
-    let docs = res.hits.hits.iter().map(|hit| hit._source.to_string()).collect();
-    Ok((docs, res._scroll_id, res.hits.total))
+    let scroll_id = extract_scroll_id(&res).unwrap().to_owned();
+    let is_finish = extract_scroll_finished(&res).unwrap();
+
+    pool.execute(move || {
+        let res: ScrollResponse = serde_json::from_str(&res).unwrap();
+        let docs = res.hits.hits.iter().map(|hit| hit._source.to_string()).collect();
+        f(Box::new(Package {
+            slice_id,
+            total: res.hits.total,
+            docs,
+        }))
+    });
+
+    Ok((scroll_id, is_finish))
+}
+
+fn extract_scroll_id(s: &str) -> Result<&str, ()> {
+    const PATTERN: &str = r#""_scroll_id":""#;
+    let start = s.find(PATTERN).unwrap() + PATTERN.len();
+    let scroll_id = &s[start..];
+    let end = scroll_id.find("\"").unwrap();
+    let scroll_id = &scroll_id[..end];
+    Ok(scroll_id)
+}
+
+fn extract_scroll_finished(s: &str) -> Result<bool, ()> {
+    const PATTERN: &str = r#""_index":""#;
+    Ok(!s.find(PATTERN).is_some())
 }
 
 #[derive(StructOpt, Debug)]
