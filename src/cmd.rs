2f6234fa6ff7b6af850695eabfe4c54957f62f82
Wenxuan Zhang <wenxuangm@gmail.com>
2019-05-22 20:22:23 +0800

    feat: Improve error handling

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,9 +1,8 @@
 use crate::cli::{CompletionOpt, Opt, PullOpt, StructOpt};
 use crate::common::Result;
-use crate::elastic::ScrollResponse;
+use crate::elastic::*;
 use crossbeam::crossbeam_channel;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
-use reqwest::Response;
 use serde_json::json;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Write};
@@ -43,8 +42,10 @@ pub fn pull(opt: PullOpt) -> Result<()> {
 
     let mpb = Arc::new(MultiProgress::new());
     let pool = threadpool::ThreadPool::new(slice as usize);
+    let (err_tx, err_rx) = crossbeam_channel::unbounded();
     for slice_id in 0..slice {
         let tx = tx.clone();
+        let err_tx = err_tx.clone();
         let mut query = query.clone();
         let host = host.clone();
         let index = index.clone();
@@ -85,83 +86,118 @@ pub fn pull(opt: PullOpt) -> Result<()> {
             if let Some(batch) = batch {
                 params.push(("size", batch.to_string()))
             }
-            let res = client
-                .post(&format!("{}/{}/_search", &host, &index))
-                .basic_auth(user.clone(), pass.clone())
-                .query(&params)
-                .json(&query)
-                .send()
-                .expect("error sending request");
 
-            let (docs, mut scroll_id, total) = parse_response(res).expect("error parsing response");
+            let res = request_elastic(
+                &client,
+                &format!("{}/{}/_search", &host, &index),
+                &query,
+                &user,
+                &pass,
+                Some(params),
+            );
 
-            let style = ProgressStyle::default_bar()
-                .template("{prefix:.bold} {elapsed_precise} {bar:50} {percent:>3}% {pos}/{len} ETA {eta_precise} {msg:.yellow.bold}")
-                .progress_chars("##-");
-            pb.set_message("Running...");
-            pb.set_style(style);
-            pb.set_length(total);
-            pb.inc(docs.len() as u64);
+            let res = match res {
+                Ok(res) => res,
+                Err(e) => {
+                    err_tx.send(format!("Fetch error[{}]: {}", slice_id, e))
+                        .expect("error sending to channel");
+                    return;
+                }
+            };
 
-            let mut finished = docs.is_empty();
-            tx.send(Box::new(docs)).expect("error sending result to channel");
+            let (mut finished, mut scroll_id) = match parse_response(res) {
+                Ok((docs, scroll_id, total)) => {
+                    let style = ProgressStyle::default_bar()
+                        .template("{prefix:.bold} {elapsed_precise} {bar:50} {percent:>3}% {pos}/{len} ETA {eta_precise} {msg:.yellow.bold}")
+                        .progress_chars("##-");
+                    pb.set_message("Running...");
+                    pb.set_style(style);
+                    pb.set_length(total);
+                    pb.inc(docs.len() as u64);
+
+                    let finished = docs.is_empty();
+                    tx.send(Box::new(docs)).expect("error sending result to channel");
+                    (finished, scroll_id)
+                }
+                Err(e) => {
+                    err_tx.send(format!("Parse error[{}]: {}", slice_id, e))
+                        .expect("error sending to channel");
+                    return;
+                }
+            };
 
             while !finished {
-                let res = client
-                    .post(&format!("{}/_search/scroll", &host))
-                    .basic_auth(user.clone(), pass.clone())
-                    .json(&json!({
+                let res = request_elastic(
+                    &client,
+                    &format!("{}/_search/scroll", &host),
+                    &json!({
                         "scroll": scroll_ttl,
                         "scroll_id": scroll_id,
-                    }))
-                    .send()
-                    .expect("error sending request");
+                    }),
+                    &user,
+                    &pass,
+                    None,
+                );
 
-                let (docs, new_scroll_id, total) = parse_response(res).expect("error parsing response");
-
-                scroll_id = new_scroll_id;
-                pb.set_length(total);
-                pb.inc(docs.len() as u64);
-                finished = docs.is_empty();
-                tx.send(Box::new(docs)).expect("error sending result to channel");
+                let res = match res {
+                    Ok(res) => res,
+                    Err(e) => {
+                        err_tx.send(format!("Error[{}]: {}", slice_id, e))
+                            .expect("error sending to channel");
+                        return;
+                    }
+                };
+                match parse_response(res) {
+                    Ok((docs, new_scroll_id, total)) => {
+                        scroll_id = new_scroll_id;
+                        pb.set_length(total);
+                        pb.inc(docs.len() as u64);
+                        finished = docs.is_empty();
+                        tx.send(Box::new(docs)).expect("error sending to channel");
+                    }
+                    Err(e) => {
+                        err_tx.send(format!("Error[{}]: {}", slice_id, e))
+                            .expect("error sending to channel");
+                        return;
+                    }
+                }
             }
 
             let style = ProgressStyle::default_bar()
                 .template("{prefix:.bold} {elapsed_precise} {bar:50} {percent:>3}% {pos}/{len} ETA {eta_precise} {msg:.green.bold}")
                 .progress_chars("##-");
             pb.set_style(style);
-            pb.finish_with_message("Finished.")
+            pb.finish_with_message("Finished.");
         });
     }
 
     thread::spawn(move || {
         pool.join();
         drop(tx);
+        drop(err_tx);
     });
 
     let output = output;
     let output_thread = thread::spawn(move || {
-        let mut output = BufWriter::new(File::create(output).unwrap());
+        let mut output = BufWriter::new(File::create(output)?);
         for docs in rx.iter() {
             for doc in docs.iter() {
-                writeln!(&mut output, "{}", doc).unwrap();
+                match writeln!(&mut output, "{}", doc) {
+                    Ok(_) => {}
+                    Err(e) => return Err(Box::new(e)),
+                }
             }
         }
+        Ok(())
     });
 
     mpb.join()?;
-    output_thread.join().unwrap();
-    Ok(())
-}
+    output_thread.join().unwrap()?;
 
-fn parse_response(mut res: Response) -> Result<(Vec<String>, String, u64)> {
-    if res.status() != 200 {
-        return Err(format!("error query es. status={}, content={}", res.status(), res.text()?).into());
+    // print error if any
+    for err in err_rx {
+        eprintln!("{}", err)
     }
-    // serde_json has bad performance on reader. So we first read body into a string.
-    // See: https://github.com/serde-rs/json/issues/160
-    let res = res.text()?;
-    let res: ScrollResponse = serde_json::from_str(&res)?;
-    let docs = res.hits.hits.iter().map(|hit| hit._source.to_string()).collect();
-    Ok((docs, res._scroll_id, res.hits.total))
+
+    Ok(())
 }
--- src/elastic.rs
+++ src/elastic.rs
@@ -1,5 +1,8 @@
+use crate::common::Result;
+use reqwest::{Client, Response};
 use serde_derive::*;
 use serde_json;
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 pub struct ScrollResponse {
@@ -18,4 +21,32 @@ pub struct Hits {
 #[derive(Serialize, Deserialize)]
 pub struct Hit {
     pub _source: serde_json::Value,
+}
+
+pub fn parse_response(mut res: Response) -> Result<(Vec<String>, String, u64)> {
+    let res = res.text()?;
+    // serde_json has bad performance on reader. So we first read body into a string.
+    // See: https://github.com/serde-rs/json/issues/160
+    let res: ScrollResponse = serde_json::from_str(&res)?;
+    let docs = res.hits.hits.iter().map(|hit| hit._source.to_string()).collect();
+    Ok((docs, res._scroll_id, res.hits.total))
+}
+
+pub fn request_elastic(
+    client: &Client,
+    url: &str,
+    query: &Value,
+    user: &str,
+    pass: &Option<String>,
+    params: Option<Vec<(&str, String)>>,
+) -> Result<Response> {
+    let res = client.post(url).basic_auth(user.clone(), pass.clone()).json(query);
+
+    let res = match params {
+        Some(params) => res.query(&params),
+        _ => res,
+    };
+
+    let res = res.send()?.error_for_status()?;
+    Ok(res)
 }
