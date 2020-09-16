69a253d2813e03066da5eb8f86571fcaf6faaf12
Wenxuan Zhang <wenxuangm@gmail.com>
2020-09-16 14:23:45 +0800

    ci: fix musl

--- ci/script.bash
+++ ci/script.bash
@@ -5,13 +5,15 @@ set -ex
 # Incorporate TARGET env var to the build and test process
 if [[ $TARGET == x86_64-unknown-linux-musl ]]; then
     # TODO: run test after build
-    docker run -v "$PWD:/volume" "clux/muslrust:$DOCKER_RUST_VERSION" cargo build --target "$TARGET" --release
+    docker run -v "$PWD:/volume" "clux/muslrust:$DOCKER_RUST_VERSION" sh -c \
+        "cargo build --target $TARGET --release && \
+         cargo install cargo-strip && \
+         cargo strip"
 else
     # We cannot run arm executables on linux
     if [[ $TARGET != arm-unknown-linux-* ]]; then
         cargo test --target "$TARGET"
     fi
     cargo build --target "$TARGET" --release
+    cargo install cargo-strip && cargo strip
 fi
-
-cargo install cargo-strip && cargo strip
